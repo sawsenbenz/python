bb8ff7b82d15185cd5e73e69ad97bb5e2d55bbcb
saw benz <benzazasawsen4@gmail.com>
2021-10-15 19:43:24 +0200

    first commit

---
+++ script_01.py
@@ -0,0 +1,2 @@
+nom=input(entrez votre nom svp: )
+age=int(input(entre votre age svp: ))
---
+++ script_02.py
@@ -0,0 +1,7 @@
+x=int(input(entre la valeur de x: ))
+y=int(input(entre la valeur de y: ))
+somme=x+y
+if (x and y)== null or (x and y)==0:
+	print("valeur non acceptes")
+else:
+	print(somme)
