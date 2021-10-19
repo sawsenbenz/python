0c35661d53043e13f6d74f9d936883d421fe4493
Bendjelloul Noureddine <bendjelloulnoureddine@gmail.com>
2021-10-19 06:58:24 +0100

    Fix indentation to 4 & change liste to int rather than Str

--- script_12.py
+++ script_12.py
@@ -1,17 +1,16 @@
 def PairImpair(liste):
-	listePairs = []
-	listeImpairs = []
-	for i in liste:
-		if ( i%2 == 0):
-			listePairs.append(i)
-		else:
-			listeImpairs.append(i)
-	
-	print(" la liste de nombre pairs est : ", listePairs)
-	print(" la liste de nombre impairs est : ", listeImpairs)		
+    listePairs = []
+    listeImpairs = []
+    for i in liste:
+        if ( i%2 == 0):
+            listePairs.append(i)
+        else:
+            listeImpairs.append(i)
+    print(" la liste de nombre pairs est : ", listePairs)
+    print(" la liste de nombre impairs est : ", listeImpairs)		
 
 
-
-liste = ['0','1','2','3','4','5','6','7','8','9']
-
+# la liste est composé de String et non pas de valeur numerique
+# liste = ['0','1','2','3','4','5','6','7','8','9']
+liste = [0,1,2,3,4,5,6,7,8,9,10]
 PairImpair(liste)
